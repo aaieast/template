7abab6cffc72313d3c6ad3f098ae66efc8fa4ec0
aaieast <aaieast@outlook.com>
2025-11-01 19:07:44 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+import { DurableObject } from "cloudflare:workers";
+export class MyDurableObject extends DurableObject {
+  constructor(ctx, env) {
+    // Required, as we are extending the base class.
+    super(ctx, env);
+  }
+
+  async sayHello() {
+    let result = this.ctx.storage.sql
+      .exec("SELECT 'Hello, World!' as greeting")
+      .one();
+    return result.greeting;
+  }
+}
+export default {
+  async fetch(request, env, ctx) {
+    const stub = env.MY_DURABLE_OBJECT.getByName(new URL(request.url).pathname);
+
+    const greeting = await stub.sayHello();
+
+    return new Response(greeting);
+  },
+};
